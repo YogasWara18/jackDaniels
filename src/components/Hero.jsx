5667c448dcd7dfea80496247ce5bf83069b65973
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-11-23 01:23:28 +0700

    Update logo

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section id="hero" className="noisy">
+        <h1 className="title">JackDaniels</h1>
+    </section>
+  )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
-import { navlinks } from "../../constants"
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react"
+
+import { navLinks } from "../../constants"
 
 const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
+
+        navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+            backgroundColor: '#00000050',
+            backgorundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power.inOut' 
+        });
+    })
+
+
   return (
     <nav>
         <div>
             <a href="#home" className="flex items-center gap-2">
-                <img src="/images/logo.png" alt="logo" />
-                JackDaniels
+                <img src="/images/logo.png" alt="logo" width="35"/>
+                <p>JackDaniels</p>
             </a>
 
             <ul>
-                {navlinks.map((link) => (
+                {navLinks.map((link) => (
                     <li key={link.id}>
                         <a href={'#${link.id}'}>{link.title}</a>
                     </li>
