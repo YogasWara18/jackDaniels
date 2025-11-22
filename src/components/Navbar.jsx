61dd7fe13e620d1f4cfc75149a3983801caa13e3
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-11-23 00:00:22 +0700

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { navlinks } from "../../constants"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <a href="#home" className="flex items-center gap-2">
+                <img src="/images/" alt="" />
+                JackDaniels
+            </a>
+
+            <ul>
+                {navlinks.map((link) => (
+                    <li key={link.id}>
+                        <a href={'#${link.id}'}>{link.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
